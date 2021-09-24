ace1fc84567c139a56299516d1c8031eb85ddee3
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-09-24 13:09:44 -0400

    Updating

--- Project_2/recent_search.py
+++ Project_2/recent_search.py
@@ -1,27 +1,27 @@
 import requests
 import os
 import json
+# For displaying the data after
+import pandas as pd
+import csv
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 #Parameters
-start_time = "2021-09-01T00:00:00.040Z"
+start_time = "2021-09-18T00:00:00.040Z"
 end_time = "2021-03-31T00:00:00.000Z"
-max_results = 10
+max_results = 25
+keyword = "(#beer OR #brewery OR #craftbeer) (Boston OR Somerville OR Cambridge) lang:en -is:retweet"
 
 search_url = "https://api.twitter.com/2/tweets/search/recent"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': 'Boston AND Beer AND Fall ',
+query_params = {'query': keyword,
                 'start_time':start_time,
                 'max_results': max_results,
-                'tweet.fields': 'geo',
-                'place':'boston'
-                #'expansions': 'geo.place_id',
-                #'user.fields':'location'
                 }
 
 
@@ -44,6 +44,10 @@ def connect_to_endpoint(url, params):
 
 def main():
     json_response = connect_to_endpoint(search_url, query_params)
+
+    df = pd.DataFrame(json_response['data'])
+    df.to_csv('response_python.csv')
+
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
