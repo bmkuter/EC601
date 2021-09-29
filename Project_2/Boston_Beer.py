2c43387d1abe096826cbf82e5e5ce4101a68439b
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-09-29 15:08:55 -0400

    Adding features

--- Project_2/Boston_Beer.py
+++ Project_2/Boston_Beer.py
@@ -4,16 +4,20 @@ import json
 # For displaying the data after
 import pandas as pd
 import csv
+# Imports the Google Cloud client library
+# https://cloud.google.com/natural-language/docs/reference/libraries#cloud-console
+from google.cloud import language_v1
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 #Parameters
-start_time = "2021-09-18T00:00:00.040Z"
+start_time = "2021-09-26T00:00:00.040Z"
 end_time = "2021-03-31T00:00:00.000Z"
 max_results = 25
 keyword = "(#beer OR #brewery OR #craftbeer) (Boston OR Somerville OR Cambridge) lang:en -is:retweet"
+example_text = "Hello World!"
 
 search_url = "https://api.twitter.com/2/tweets/search/recent"
 
@@ -23,6 +27,10 @@ query_params = {'query': keyword,
                 'start_time':start_time,
                 'max_results': max_results,
                 }
+
+# Instantiates a client
+client = language_v1.LanguageServiceClient()
+
 
 
 def bearer_oauth(r):
@@ -48,7 +56,22 @@ def main():
     df = pd.DataFrame(json_response['data'])
     df.to_csv('response_python.csv')
 
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    #print(json.dumps(json_response, indent=4, sort_keys=True))
+
+    df = pd.read_csv('response_python.csv')
+    saved_column = df.text
+
+    #For each text in the csv, perform sentiment analysis
+    print("\n\n\n\n\n\n")   #Formatting
+    for i in saved_column:
+        #Google Natural Language Section
+        document = language_v1.Document(content=i, type_=language_v1.Document.Type.PLAIN_TEXT)
+        sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+        #Only Print if sentiment score is good, i.e. above 0.5?
+        if (sentiment.score >= .3) & (sentiment.magnitude > 1):
+            print("Text: {}".format(i))
+            print("Sentiment: Score {}, Magnitude {}".format(sentiment.score, sentiment.magnitude))
+            print("\n\n\n\n\n\n")
 
 
 if __name__ == "__main__":
--- Project_2/README.md
+++ Project_2/README.md
@@ -11,3 +11,7 @@ To start I copied several of the python example codes for the Twitter API v2 thr
 After writing the above paragraph I realised I must be missing something, as this type of Boolean search seems very important. After delving into the API even further, I think I have found a section of operators for keywords that enable the functionality I so desire [[here](https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query#list)].  
 
 I then added a *.csv function to export the JSON as a csv, so that it can later be made into a sortable list through Google's NLP.  
+
+To prepare for adding Google's NLP, I changed the name of my aforementioned sample file to "Boston_Beer.py", and added include statements for the required Google NLP API.
+
+After adding Google NLP API, the program now takes the text found for each tweet and saves to CSV (which acts as a way to format data into an easier format than JSON). This formatted text is then fed back into the program. Sentiment analysis is performed on each, and the analysis returned along with the original text. The tweet is only printed if generally positive (sentiment score >= .3) and the magnitude is greater than 1 (used to filter out spam messages and really lazy ads). 
